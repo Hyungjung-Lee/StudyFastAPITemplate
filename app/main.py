1157d32ada8df075974f8cab5d042e909e09af95
lhj5772 <>
2021-08-29 22:52:25 +0900

    [feature/api_simple_day1] add api types

--- app/main.py
+++ app/main.py
@@ -1,6 +1,14 @@
-from typing import Optional
+from typing import Optional, List
 
-from fastapi import FastAPI
+from fastapi import FastAPI, Header
+
+from pydantic import BaseModel
+
+class Item(BaseModel):
+    name: str
+    description: Optional[str] = None
+    price: float
+    tax: Optional[float] = None
 
 app = FastAPI()
 
@@ -8,7 +16,20 @@ app = FastAPI()
 def read_root():
     return {"Hello": "World"}
 
-
 @app.get("/items/{item_id}")
-def read_item(item_id: int, q: Optional[str] = None):
+def read_item(item_id: str, q: Optional[str] = None):
     return {"item_id": item_id, "q": q}
+
+@app.put("/items/{item_id}")
+def put_item(item_id: str, q: Optional[str] = None):
+    return {"item_id": item_id, "q": q}
+
+@app.post("/items/{item_id}")
+def post_item(item_id: str, x_token: Optional[List[str]] = Header(None), item: Item = Item(name = "", price = 0.0)):
+    return {"item_id": item_id, "x_token": x_token, "body": item}
+
+@app.delete("/items/{item_id}")
+def delete_item(item_id: str, x_token: Optional[List[str]] = Header(None)):
+    return {"item_id": item_id, "x_token": x_token}
+
+
